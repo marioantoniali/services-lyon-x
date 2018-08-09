c0729a59f4be24df13c9197620213d9e38eed936
willy <wilg1569@gmail.com>
2018-08-09 14:51:36 +0200

    Save to db

--- app/services/start_scrap.rb
+++ app/services/start_scrap.rb
@@ -9,11 +9,13 @@ class StartScrap
     @address = address
   end
 
+  # Méthode qui exécute les actions
   def perform
     scrap_crypto
     save
   end
 
+  # Méthode pour scraper notre page web de cryptomonnaies
   def scrap_crypto
     page = Nokogiri::HTML(open(@address))
       crypto_name = page.css('a.currency-name-container').map { |crypto| crypto.text }
@@ -22,7 +24,8 @@ class StartScrap
     @my_hash = crypto_name.zip(crypto_price)
   end
 
+  # Méthode pour enregistrer les résultats du scraping dans notre BDD SQlite3
   def save
-    @my_hash.each { |name, price| Crypto.create(name: name, value: price) }
+    @my_hash.each { |name, price| Crypto.create(name: name, value: price) } # On applique la méthode create sur notre model Crypto pour enregistrement en BDD (le model fait le lien)
   end
 end
