fbd7cf03d09f858a336756d980ab6717efbfa0b3
willy <wilg1569@gmail.com>
2018-08-09 19:58:20 +0200

    Terminé

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,4 +1,5 @@
 class CryptosController < ApplicationController
+  # Méthode si on souhaite utiliser un text-field pour la recherche
   def index
     @search = params[:search]
     @cryptos = Crypto.find_by(name: @search)
